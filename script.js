9e6bb444ebb2f0498c7f1d4c0dcfb9bc505d0803
cheddar-design <itslegoman823@gmail.com>
2023-03-23 21:07:59 -0500

    bug fixes

--- script.js
+++ script.js
@@ -252,7 +252,7 @@ const emoEncode = (str) => {
         if (activated) {
             return getEmo(c);
         } else {
-            return getLetter((chars.get(emoChars.get(c))+Math.floor(Math.random() * 10))%cl)
+            return getEmo(getLetter((chars.get(c)+Math.floor(Math.random() * 10))%cl))
         }
     });
     return encoded.join('');
